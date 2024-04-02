15839c6c78819c8d906fd140893cb7f2acdfd404
evelyn1028 <yoons925@newschool.edu>
2024-04-02 00:53:35 -0400

    Project2 Final

---
+++ project2/js/capsule_page7.js
@@ -0,0 +1,42 @@
+let container = document.getElementById("container");
+let initialPageWidth, initialPageHeight;
+
+window.onload = function() {
+    initialPageWidth = window.innerWidth;
+    initialPageHeight = window.innerHeight;
+    container.style.width = initialPageWidth + 'px';
+    container.style.height = initialPageHeight + 'px';
+    // container.style.overflow = "hidden";
+};
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    let img = document.getElementById(data);
+    let base = document.getElementsByClassName("base");
+    let base_frame = base[0];
+    if (container.contains(event.target)) {
+        let rect = base_frame.getBoundingClientRect();
+        let offsetX = event.clientX;
+        let offsetY =  event.clientY;
+
+        if ((rect.top <= offsetY && offsetY <= rect.top + rect.height) &&
+            (rect.left <= offsetX && offsetX <= rect.left + rect.width)) {
+            img.removeAttribute("id");
+            img.style.scale = "0.5";
+            img.style.left = offsetX - img.width / 2 + "px";
+            img.style.top = offsetY - img.height / 2 + "px";
+    
+            img.style.position = "absolute";
+            container.appendChild(img);
+        }
+    }
+}
